2f168d4f2c1d1e50f5a589acfe0cd0c092faa3a5
binding-archive-7591 <yarhumher@hotmail.com>
2020-03-31 03:05:11 +0000

    Automatically backed up by Learn

--- lib/car.rb
+++ lib/car.rb
@@ -1,14 +1,17 @@
 require_relative "./vehicle.rb"
-class Car
-  attr_accessor :wheel_size, :wheel_number
-  def initialize(wheel_size,wheel_number = 2 )
-    @wheel_size = wheel_size
-    @wheel_number = wheel_number
-  end
-  def go
-    "VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!"
-  end
-  def fill_up_tank
-  "filling up!"
- end
+# class Car
+#   attr_accessor :wheel_size, :wheel_number
+#   def initialize(wheel_size,wheel_number = 2 )
+#     @wheel_size = wheel_size
+#     @wheel_number = wheel_number
+#   end
+#   def go
+#     "VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!"
+#   end
+#   def fill_up_tank
+#     "filling up!"
+#   end
+# end
+class Car < Vehicle
+ 
 end
