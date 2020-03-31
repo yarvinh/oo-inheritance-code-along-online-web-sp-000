0d054a573237ad97e3e197b8d605094f1e754721
binding-archive-7591 <yarhumher@hotmail.com>
2020-03-31 02:43:23 +0000

    Automatically backed up by Learn

--- lib/car.rb
+++ lib/car.rb
@@ -1,1 +1,9 @@
 require_relative "./vehicle.rb"
+class car
+  attr_accessor :wheel_size, :number
+  def initialize(wheel_size,number)
+    @wheel_size = wheel_size
+    @number = number
+  end
+
+end
